2acdc0570bf3b3c1c6dd7362914da9a677f22153
Dimpi <khandelwaldimpi6@gmail.com>
2024-05-16 21:24:13 +0530

    donee

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// gsap.to("#box1",{
-//     x:1000,
-//     duration:2,
-//     delay: 1,
-//     borderRadius:"50%",
-//     backgroundColor:"yellow",
-//     rotate:360
-// })
-
-// gsap.from("h1",{
-//     opacity:0,
-//     y:30,
-//     duration:2,
-//     delay: 1,
-//     // color: "pink",
-//     stagger: 1
-// })
-
-// gsap.to("#box",{
-//     x:1000,
-//     duration:2,
-//     delay: 1,
-//     // borderRadius:"50%",
-//     // backgroundColor:"yellow",
-//     rotate:360,
-//     repeat:-1,
-//     yoyo:true
-// })
-
 var tl = gsap.timeline()
 
 tl.from ("img",{
